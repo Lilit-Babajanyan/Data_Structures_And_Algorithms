890dc806cbd55d5bb0fe480b51cac90cf085d6fc
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-07-01 01:08:13 +0400

    Bubble Sort

---
+++ Algorithms/bubble_sort.js
@@ -0,0 +1,19 @@
+function bubble_sort(arr) {
+  let n = arr.length;
+
+  for (let pass = 0; pass < n - 1; pass++) {
+    let swapped = false;
+    for (let i = 0; i < n - 1 - pass; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return arr;
+}
